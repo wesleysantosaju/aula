0ab2af3462dca7656c459c892baa996bee484449
wesleysantos <wvsantos15@gmail.com>
2023-12-27 10:00:45 -0300

    config h2database for test application

--- src/main/java/com/curso/alura/aula/models/Usuario.java
+++ src/main/java/com/curso/alura/aula/models/Usuario.java
@@ -1,13 +1,23 @@
 package com.curso.alura.aula.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = Usuario.TABLE_NAME)
 public class Usuario {
 
     public static final String TABLE_NAME = "user";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", length = 60, nullable = false)
+    private String password;
 
 }
