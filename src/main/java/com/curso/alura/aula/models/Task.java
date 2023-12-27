8d1ebf7399bd2b4f97b8ad95ebdb4aa5e5677180
wesleysantos <wvsantos15@gmail.com>
2023-12-27 12:07:09 -0300

    adding class for insert tasks

---
+++ src/main/java/com/curso/alura/aula/models/Task.java
@@ -0,0 +1,89 @@
+package com.curso.alura.aula.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = Task.TABLE_NAME)
+public class Task {
+
+    public static final String TABLE_NAME = "task";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    private Usuario user;
+
+    @Column(name = "description", length = 255, nullable = false)
+    @NotNull
+    @NotEmpty
+    @Size(min = 1, max = 255)
+    private String descriprion;
+
+    public Task() {
+    }
+
+    public Task(Long id, Usuario user, String descriprion) {
+        this.id = id;
+        this.user = user;
+        this.descriprion = descriprion;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Usuario getUser() {
+        return user;
+    }
+
+    public void setUser(Usuario user) {
+        this.user = user;
+    }
+
+    public String getDescriprion() {
+        return descriprion;
+    }
+
+    public void setDescriprion(String descriprion) {
+        this.descriprion = descriprion;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (o == null)
+            return false;
+        if (! (o instanceof Task other))
+            return false;
+
+        if(this.id == null)
+            if(other.id != null)
+                return false;
+            else return false;
+
+        return Objects.equals(this.id, other.id) && Objects.equals(this.user, other.user)
+                && Objects.equals(this.descriprion, other.descriprion);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        return result;
+    }
+}
