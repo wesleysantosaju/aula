c17202124a3dad27660f042da9facbbb01c11dea
wesleysantos <wvsantos15@gmail.com>
2023-12-27 12:44:54 -0300

    Naming correction

--- src/main/java/com/curso/alura/aula/models/Usuario.java
+++ src/main/java/com/curso/alura/aula/models/Usuario.java
@@ -41,7 +41,7 @@ public class Usuario {
     @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
     private String password;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "usuario")
     private List<Task> tasks = new ArrayList<Task>();
 
     public Usuario(){}
