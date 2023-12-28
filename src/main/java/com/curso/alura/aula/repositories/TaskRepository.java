18932df8bc3cb1caf3a862bfa598de7b00b97caa
wesleysantos <wvsantos15@gmail.com>
2023-12-28 10:54:00 -0300

    remove imports

--- src/main/java/com/curso/alura/aula/repositories/TaskRepository.java
+++ src/main/java/com/curso/alura/aula/repositories/TaskRepository.java
@@ -2,14 +2,10 @@ package com.curso.alura.aula.repositories;
 
 
 import com.curso.alura.aula.models.Task;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
